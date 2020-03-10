8627ba396d4626921ea4cf3bbe2f5f6bbdf05b79
danil31219as <57491208+danil31219as@users.noreply.github.com>
2020-03-10 19:41:00 +0400

    Add files via upload

---
+++ login_password_web/index.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Id астронавта', validators=[DataRequired()])
+    password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    username_2 = StringField('Id капитана', validators=[DataRequired()])
+    password_2 = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('form.html', title='Авторизация', form=form)
+
+
+@app.route('/success')
+def success():
+    return 'OK'
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
